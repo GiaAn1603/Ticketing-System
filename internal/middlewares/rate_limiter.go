3843fec560459a6bfd69b54b2d12cd7b362bd0b8
GiaAn1603 <anphamproductions@gmail.com>
2026-02-26 23:33:22 +0700

    feat: add rate limiter middleware

---
+++ internal/middlewares/rate_limiter.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+
+	"Ticketing-System/scripts"
+)
+
+const (
+	luaSuccess           = 1
+	luaInvalidInput      = -1000
+	luaRateLimitExceeded = -3001
+)
+
+type RateLimiter struct {
+	rdb       *redis.Client
+	capacity  int
+	rate      int
+	timeout   time.Duration
+	scriptSHA string
+}
+
+func NewRateLimiter(ctx context.Context, rdb *redis.Client, capacity, rate int, timeout time.Duration) (*RateLimiter, error) {
+	sha, err := rdb.ScriptLoad(ctx, scripts.RateLimitScript).Result()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load rate_limit lua script: %w", err)
+	}
+
+	log.Printf("[MIDDLEWARE][INFO] Lua script loaded successfully | sha=%s", sha)
+
+	return &RateLimiter{
+		rdb:       rdb,
+		capacity:  capacity,
+		rate:      rate,
+		timeout:   timeout,
+		scriptSHA: sha,
+	}, nil
+}
